575166bb72577ebd610ebd64dad18807c426f2f5
Nicole Hernandez <Nicoledbz>
2022-07-24 07:27:41 -0400

    dq py

---
+++ google_python/dq4.py
@@ -0,0 +1,12 @@
+# What’s a major advantage of using dictionaries over lists?
+
+ # Dictionaries are ordered sets
+
+
+ # Dictionaries can be accessed by the index number of the element
+
+
+# Elements can be removed and inserted into dictionaries
+
+
+# It’s quicker and easier to find a specific element in a dictionary
